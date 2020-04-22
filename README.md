550f20c8d6e317e267dbb45897be44da164852fa
PAVEL TKACHEV <phoenixweiss@users.noreply.github.com>
2020-04-23 00:12:52 +0300

    prettify all

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Bookshelf
+
+<img src="./bookshelf_logo.svg" width="128">
+
+A free and open source bookshelf organizer. Built with _Electron_, _React_ & :heart:
--- index.js
+++ index.js
@@ -1,54 +1,50 @@
-const {
-  app,
-  BrowserWindow
-} = require('electron')
-const Store = require('electron-store');
+const { app, BrowserWindow } = require("electron");
+const Store = require("electron-store");
 const store = new Store();
-const debug = /--debug/.test(process.argv[2])
+const debug = /--debug/.test(process.argv[2]);
 
-let mainWindow = null
+let mainWindow = null;
 
-store.set('unicorn', 'Azaza');
-console.log(store.get('unicorn'));
+store.set("unicorn", "Azaza");
+console.log(store.get("unicorn"));
 
 function initialize() {
-
   function createWindow() {
     mainWindow = new BrowserWindow({
       width: 800,
       height: 600,
       webPreferences: {
         nodeIntegration: true,
-        allowRunningInsecureContent: false
-      }
-    })
+        allowRunningInsecureContent: false,
+      },
+    });
 
-    mainWindow.loadFile('index.html')
+    mainWindow.loadFile("index.html");
 
     if (debug) {
-      mainWindow.webContents.openDevTools()
-      mainWindow.maximize()
-      require('devtron').install()
+      mainWindow.webContents.openDevTools();
+      mainWindow.maximize();
+      require("devtron").install();
     }
 
-    mainWindow.on('closed', () => {
-      mainWindow = null
-    })
+    mainWindow.on("closed", () => {
+      mainWindow = null;
+    });
   }
 
-  app.on('ready', createWindow)
+  app.on("ready", createWindow);
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
     }
-  })
+  });
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
+  });
 }
 
-initialize()
+initialize();
